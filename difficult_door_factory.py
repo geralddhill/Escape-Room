687c0a7e1e7e4e4255b0591b164ad1d382b4dcb6
Ben2104 <dohoangkhoi341@gmail.com>
2024-11-04 13:43:25 -0800

    updates difficult_door_factory.py

--- difficult_door_factory.py
+++ difficult_door_factory.py
@@ -1,14 +1,29 @@
-from door import Door
-
-class DiffucultDoorFactory(Door):
+from door_factory import DoorFactory
+import random
+from combo_door import ComboDoor
+from deadbolt_door import DeadboltDoor
+from code_door import CodeDoor
+class DiffucultDoorFactory(DoorFactory):
     """Door factory that creates an instance or a random diffucult door.
     """
 
-    def create_door(self) -> Door:
+    def create_door(self):
         """Factory method to create an instance of a door.
 
         Chooses randomly between ComboDoor, DeadboltDoor, and CodeDoor.
 
         :return: Door instance
         """
+        door_choice = random.randint(1,3)
         
+        if door_choice == 1:
+            # return ComboDoor
+            return ComboDoor()
+        elif door_choice == 2:
+            # return DeadboltDoor
+            return DeadboltDoor()
+        else:
+            # return CodeDoor
+            return CodeDoor()
+d = DiffucultDoorFactory()
+print(d.create_door())
